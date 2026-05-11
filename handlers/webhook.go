97b72a9dc5722f60f550e09be57cedd4cb059da6
Scott Wisniewski <scott@agile.security>
2026-05-11 16:23:29 -0700

    Add ParseWebHook method.

---
+++ handlers/webhook.go
@@ -0,0 +1,31 @@
+package handlers
+
+import "github.com/google/go-github/v81/github"
+
+func ParseWebHook(deliverID string, messageType string, payload []byte) (Event, error) {
+	event, err := github.ParseWebHook(messageType, payload)
+	if err != nil {
+		return nil, err
+	}
+
+	switch event := event.(type) {
+	case *github.InstallationEvent:
+		return webhookToInstallation(deliverID, event), nil
+	default:
+		return nil, ErrUnknownEvent
+	}
+}
+
+func webhookToInstallation(deliveryID string, evt *github.InstallationEvent) *InstallationEvent {
+	return &InstallationEvent{
+		EventBase: EventBase{DeliveryID: deliveryID},
+		Action:    evt.GetAction(),
+		Installation: Installation{
+			ID:       evt.GetInstallation().GetID(),
+			AppID:    evt.GetInstallation().GetAppID(),
+			AppSlug:  evt.GetInstallation().GetAppSlug(),
+			OrgLogin: evt.GetInstallation().GetAccount().GetLogin(),
+			OrgID:    evt.GetInstallation().GetAccount().GetID(),
+		},
+	}
+}
